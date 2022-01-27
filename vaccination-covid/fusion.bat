ba1ee5ac73e2289f32b5349420c8a076e4e6a657
LouisPlu <louis.pluviose@uha.fr>
2022-01-27 11:13:42 +0100

    commit

--- vaccination-covid/fusion.bat
+++
@@ -1,1 +0,0 @@
-copy *csv masterlist.csv
--- vaccination-covid/src/model/model.py
+++ vaccination-covid/src/model/model.py
@@ -1,11 +1,46 @@
- 
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import os
+
+df = pd.read_csv(r".\vaccination-covid\src\data\masterlist.csv", delimiter=";")
+
+#4. Déterminer pour chaque région (*src/model/model.py*) quand la vaccination à débuté.
+y = df.sort_values(by=["semaine_injection", "effectif_1_inj", "region_residence"])
+
+
+
+compression_opts = dict(method='zip', archive_name='out.csv')  
+y.to_csv('out.zip', sep=";", index=False, compression=compression_opts) 
+
+def debutVaccination():
+    numRegion = []
+    date = []
+    for df.effectif_1_inj in df.semaine_injection:
+        if df.region_residence == numRegion:
+            pass
+        else:
+            numRegion.append(df.region_residence)
+            date.append(df.semaine_injection)
+    return(numRegion, date)
 
 def ComputeMean():
     mean = 0
     return (mean)
 
-def ComputeMedian:
+def ComputeMedian():
     median = 0
     return (median)    
     
-    
+numRegion = []
+date = []
+s = range(0,100)
+for s in df.semaine_injection.iteritems():
+    if s == numRegion:
+        pass
+    else:
+        numRegion.append(df.region_residence)
+        date.append(df.semaine_injection)
+
+print(numRegion, date)
